fa052555a856cb4d97d623ba4a1492df38170837
vnjxk <archlord6@gmail.com>
2015-03-28 16:58:54 +0300

    Encrypted Chat

---
+++ EncryptedChat/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace EncryptedChat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtMsg = new System.Windows.Forms.TextBox();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.txtKey = new System.Windows.Forms.TextBox();
+            this.txtUid = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnclear = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSend
+            // 
+            this.btnSend.Enabled = false;
+            this.btnSend.Location = new System.Drawing.Point(605, 401);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 0;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtMsg
+            // 
+            this.txtMsg.Location = new System.Drawing.Point(12, 401);
+            this.txtMsg.Name = "txtMsg";
+            this.txtMsg.Size = new System.Drawing.Size(573, 20);
+            this.txtMsg.TabIndex = 1;
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(12, 99);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ReadOnly = true;
+            this.txtLog.Size = new System.Drawing.Size(668, 296);
+            this.txtLog.TabIndex = 2;
+            // 
+            // txtKey
+            // 
+            this.txtKey.Location = new System.Drawing.Point(40, 12);
+            this.txtKey.Name = "txtKey";
+            this.txtKey.Size = new System.Drawing.Size(320, 20);
+            this.txtKey.TabIndex = 3;
+            this.txtKey.Text = "abcdefghijuklmno0123456789012345";
+            // 
+            // txtUid
+            // 
+            this.txtUid.Location = new System.Drawing.Point(40, 38);
+            this.txtUid.Name = "txtUid";
+            this.txtUid.Size = new System.Drawing.Size(320, 20);
+            this.txtUid.TabIndex = 4;
+            this.txtUid.Text = "PublicChat";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Key";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(0, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "UID";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(40, 70);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(210, 23);
+            this.btnConnect.TabIndex = 7;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(526, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Tools";
+            // 
+            // btnclear
+            // 
+            this.btnclear.Location = new System.Drawing.Point(605, 25);
+            this.btnclear.Name = "btnclear";
+            this.btnclear.Size = new System.Drawing.Size(75, 23);
+            this.btnclear.TabIndex = 9;
+            this.btnclear.Text = "Clear";
+            this.btnclear.UseVisualStyleBackColor = true;
+            this.btnclear.Click += new System.EventHandler(this.btnclear_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Enabled = false;
+            this.btnClose.Location = new System.Drawing.Point(256, 70);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(104, 23);
+            this.btnClose.TabIndex = 10;
+            this.btnClose.Text = "Close Connection";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(692, 438);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnclear);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtUid);
+            this.Controls.Add(this.txtKey);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.txtMsg);
+            this.Controls.Add(this.btnSend);
+            this.Name = "Form1";
+            this.Text = "Chat";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtMsg;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.TextBox txtKey;
+        private System.Windows.Forms.TextBox txtUid;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnclear;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
+
---
+++ EncryptedChat/Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace EncryptedChat
+{
+    public partial class Form1 : Form
+    {
+        string key = string.Empty;
+        string Uid = string.Empty;
+        string mdUid = string.Empty;
+        string dateUid = string.Empty;
+        string keyTwo = "abcdefghijuklmno0123456789012345";
+        List<string> msgDates = new List<string>();
+        HttpHandler HttpDo = new HttpHandler();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            key = txtKey.Text;
+            Uid = txtUid.Text;
+            mdUid = MD5(Uid);
+            dateUid = MD5(mdUid);
+
+            toLog("Connecting...");
+            string textmsg = getContent(mdUid);
+            if (textmsg == null || textmsg == "CLOSED")
+            {
+                toLog("Connected!");
+                HttpDo.addPost(mdUid, "CLOSED", key);
+                
+            }
+            else
+            {
+                toLog("Detected UnClosed connection!");
+                toLog("Connected!");
+            }
+            InitTimer();
+            btnClose.Enabled = true;
+            btnSend.Enabled = true;
+        }
+
+        private void toLog(string p)
+        {
+            txtLog.Text += (p + "\r\n");
+        }
+
+
+        private  string getContent(string id)
+        {
+            HttpHandler httpDo = new HttpHandler();
+            if (httpDo.getPost(id) == "Error - Not found")
+            {
+                return null;
+            }
+            string content = Decrypt(httpDo.getPost(id), key);
+            string c = null;
+            try
+            {
+                c = content.TrimEnd('\0'); //removes null bytes that comes with string
+            }
+            catch (NullReferenceException) { }
+
+            return c;
+        }
+        public static string Decrypt(string toDecrypt, string key)
+        {
+            if (toDecrypt == "Error - Not found")
+            {
+                return null;
+            }
+            if (toDecrypt != null)
+            {
+
+                byte[] keyArray = UTF8Encoding.UTF8.GetBytes(key); // AES-256 key
+                byte[] toEncryptArray = Convert.FromBase64String(toDecrypt);
+                RijndaelManaged rDel = new RijndaelManaged();
+                rDel.Key = keyArray;
+                rDel.Mode = CipherMode.ECB;
+                rDel.Padding = PaddingMode.None; // better lang support
+                ICryptoTransform cTransform = rDel.CreateDecryptor();
+                byte[] resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+                return UTF8Encoding.UTF8.GetString(resultArray);
+            }
+            else { return null; }
+        } 
+
+        private static string MD5(string phase)
+        {
+
+            if (phase != null)
+            {
+                // byte array representation of that string
+                byte[] encodedPassword = new UTF8Encoding().GetBytes(phase);
+
+                // need MD5 to calculate the hash
+                byte[] hash = ((HashAlgorithm)CryptoConfig.CreateFromName("MD5")).ComputeHash(encodedPassword);
+
+                // string representation (similar to UNIX format)
+                string encoded = BitConverter.ToString(hash)
+                    // without dashes
+                   .Replace("-", string.Empty)
+                    // make lowercase
+                   .ToLower();
+                return encoded;
+            }
+            return null;
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            HttpDo.addPost(mdUid, "CLOSED", key);
+            toLog("closed!");
+            timer1.Stop();
+            btnClose.Enabled = false;
+            btnSend.Enabled = false;
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            PostMsg(txtMsg.Text);
+            txtMsg.Text = string.Empty;
+        }
+        private Timer timer1;
+        public void InitTimer()
+        {
+            timer1 = new Timer();
+            timer1.Tick += new EventHandler(timer1_Tick);
+            timer1.Interval = 1000; // in miliseconds
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+             
+         CheckMessage();
+             
+       }
+
+        private void CheckMessage()
+        {
+            string latest = getContent(dateUid);
+            string textmsg = getContent(mdUid);
+            if (latest != null)
+            {
+                if (!msgDates.Exists(element => element == latest) && textmsg != "CLOSED")
+                {
+                    msgDates.Add(latest);
+                    toLog(latest.Substring(0, 16) + " " + textmsg);
+                }
+            }
+        }
+        private void PostMsg(string text)
+        {
+            string date = DateTime.Now.ToString("dd-MM-yyyy HH:mm:ss");
+            HttpDo.addPost(mdUid, text, key);
+            HttpDo.addPost(dateUid, date, key);
+            msgDates.Add(date);
+        }
+
+        private void btnclear_Click(object sender, EventArgs e)
+        {
+            txtLog.Text = string.Empty;
+        }
+
+
+        public static string cEncrypt(string input, string key)
+        {
+            byte[] inputArray = UTF8Encoding.UTF8.GetBytes(input);
+            TripleDESCryptoServiceProvider tripleDES = new TripleDESCryptoServiceProvider();
+            tripleDES.Key = UTF8Encoding.UTF8.GetBytes(key);
+            tripleDES.Mode = CipherMode.ECB;
+            tripleDES.Padding = PaddingMode.None;
+            ICryptoTransform cTransform = tripleDES.CreateEncryptor();
+            byte[] resultArray = cTransform.TransformFinalBlock(inputArray, 0, inputArray.Length);
+            tripleDES.Clear();
+            return Convert.ToBase64String(resultArray, 0, resultArray.Length);
+        }
+        public static string cDecrypt(string input, string key)
+        {
+            try
+            {
+                input = input.TrimEnd('\0'); //removes null bytes that comes with string
+            }
+            catch (NullReferenceException) { }
+            try
+            {
+                byte[] inputArray = Convert.FromBase64String(input);
+                TripleDESCryptoServiceProvider tripleDES = new TripleDESCryptoServiceProvider();
+                tripleDES.Key = UTF8Encoding.UTF8.GetBytes(key);
+                tripleDES.Mode = CipherMode.ECB;
+                tripleDES.Padding = PaddingMode.None;
+                ICryptoTransform cTransform = tripleDES.CreateDecryptor();
+                byte[] resultArray = cTransform.TransformFinalBlock(inputArray, 0, inputArray.Length);
+                tripleDES.Clear();
+                return UTF8Encoding.UTF8.GetString(resultArray);
+            }
+            catch (System.FormatException)
+            {
+                return input;
+            }
+        }
+    }
+}
